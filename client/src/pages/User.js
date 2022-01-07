96f698fa26b81fb81a62707c35312e29cc4568b8
Hadedeji10k <adedejiyusuf26@gmail.com>
2022-01-07 21:05:34 +0100

    Fix

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -4,7 +4,7 @@ const User = (props) => {
     const { img, name } = props
     return (
         <div className="userBox">
-            <img src={img} className="image" alt={`${name}`s image}/>
+            <img src={img} className="image" alt={`${name}'s image`}/>
             {/* <a href={img}>Hey</a> */}
             <h3 className="userName">{name}</h3>
             <button><a href={`https://www.instagram.com/${name}`}>View user profile</a></button>
